a73c09a0613abf41c3d1b6163a985e8d4f2ff74e
Hans Andersen <hemae2468@gmail.com>
2021-02-13 21:54:31 +0500

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://m.dw.com/image/49947376_401.jpg" />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="http://www.prikleem.ru/published/publicdata/PRIKLE35SHOP/attachments/SC/products_pictures/3541.png" />
+            </div>
+            <div>
+                ava+descr
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post1
+                    </div>
+                    <div>
+                        post2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile;
